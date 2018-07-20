fa158731570130e011013f80f00d02de7675fde4
jelena <jelena.gruica@gmail.com>
2018-07-19 17:21:35 -0700

    added replicated-rbac, basic schema

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,5 +1,6 @@
 import * as replicatedEntitlements from "./replicated-entitlements";
 import * as replicatedSupportBundle from "./replicated-supportbundle";
 import * as replicatedShip from "./replicated-ship";
+import * as replicatedRbac from "./replicated-rbac";
 
-export { replicatedEntitlements, replicatedSupportBundle, replicatedShip };
+export { replicatedEntitlements, replicatedSupportBundle, replicatedShip, replicatedRbac };
